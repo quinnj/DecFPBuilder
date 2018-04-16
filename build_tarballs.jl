6388339fabefff4f4d24a66c88fd973904fafb45
John M Kuhn <jmkuhn@starpower.net>
2018-04-16 16:39:16 -0400

    Update to new build_tarballs style

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -4,7 +4,6 @@ using BinaryBuilder
 sources = [
     "https://software.intel.com/sites/default/files/m/d/4/1/d/8/IntelRDFPMathLib20U1.tar.gz" =>
     "dcd56b70a57d783d31c73798cabc33d715d0fdca8465725e4eebfb9de88444dd",
-
 ]
 
 # Bash recipe for building across all platforms
@@ -75,53 +74,13 @@ platforms = [
 ]
 
 # The products that we will ensure are always built
-products(prefix) = Product[
+products(prefix) = [
     LibraryProduct(prefix, "libbid", :libbid)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    
 ]
 
-# Parse out some command-line arguments
-BUILD_ARGS = ARGS
-
-# This sets whether we should build verbosely or not
-verbose = "--verbose" in BUILD_ARGS
-BUILD_ARGS = filter!(x -> x != "--verbose", BUILD_ARGS)
-
-# This flag skips actually building and instead attempts to reconstruct a
-# build.jl from a GitHub release page.  Use this to automatically deploy a
-# build.jl file even when sharding targets across multiple CI builds.
-only_buildjl = "--only-buildjl" in BUILD_ARGS
-BUILD_ARGS = filter!(x -> x != "--only-buildjl", BUILD_ARGS)
-
-if !only_buildjl
-    # If the user passed in a platform (or a few, comma-separated) on the
-    # command-line, use that instead of our default platforms
-    if length(BUILD_ARGS) > 0
-        platforms = platform_key.(split(BUILD_ARGS[1], ","))
-    end
-    info("Building for $(join(triplet.(platforms), ", "))")
-
-    # Build the given platforms using the given sources
-    autobuild(pwd(), "DecFP", platforms, sources, script, products;
-                                      dependencies=dependencies, verbose=verbose)
-else
-    # If we're only reconstructing a build.jl file on Travis, grab the information and do it
-    if !haskey(ENV, "TRAVIS_REPO_SLUG") || !haskey(ENV, "TRAVIS_TAG")
-        error("Must provide repository name and tag through Travis-style environment variables!")
-    end
-    repo_name = ENV["TRAVIS_REPO_SLUG"]
-    tag_name = ENV["TRAVIS_TAG"]
-    product_hashes = product_hashes_from_github_release(repo_name, tag_name; verbose=verbose)
-    bin_path = "https://github.com/$(repo_name)/releases/download/$(tag_name)"
-    dummy_prefix = Prefix(pwd())
-    print_buildjl(pwd(), products(dummy_prefix), product_hashes, bin_path)
-
-    if verbose
-        info("Writing out the following reconstructed build.jl:")
-        print_buildjl(STDOUT, product_hashes; products=products(dummy_prefix), bin_path=bin_path)
-    end
-end
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, "DecFP", sources, script, platforms, products, dependencies)
