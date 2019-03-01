5a88300843a9034c518e4b7c815ae02e2d0e3028
Simon Byrne <simonbyrne@gmail.com>
2019-02-28 16:07:11 -0800

    Update to newer release

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -2,8 +2,8 @@ using BinaryBuilder
 
 # Collection of sources required to build DecFP
 sources = [
-    "https://software.intel.com/sites/default/files/m/d/4/1/d/8/IntelRDFPMathLib20U1.tar.gz" =>
-    "dcd56b70a57d783d31c73798cabc33d715d0fdca8465725e4eebfb9de88444dd",
+    "http://www.netlib.org/misc/intel/IntelRDFPMathLib20U2.tar.gz" =>
+    "93c0c78e0989df88f8540bf38d6743734804cef1e40706fd8fe5c6a03f79e173",
 ]
 
 # Bash recipe for building across all platforms
@@ -11,7 +11,7 @@ script = raw"""
 if [ $target == "x86_64-w64-mingw32" ]; then
 
 cd $WORKSPACE/srcdir
-cd IntelRDFPMathLib20U1/LIBRARY/src/
+cd IntelRDFPMathLib20U2/LIBRARY/src/
 wget https://raw.githubusercontent.com/weolar/miniblink49/master/vc6/include/crt/float.h
 cd ..
 make CC_NAME_INDEX=3 CC_INDEX=3 _HOST_OS=Windows_NT _HOST_ARCH=x86_64 _NUM_CPUS=1 CC=x86_64-w64-mingw32-gcc CFLAGS_OPT="-O2 -DBID_THREAD= -DBID_MS_FLAGS" CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
@@ -22,7 +22,7 @@ cp libbid.dll $prefix/bin/.
 elif [ $target == "i686-w64-mingw32" ]; then
 
 cd $WORKSPACE/srcdir
-cd IntelRDFPMathLib20U1/LIBRARY/src/
+cd IntelRDFPMathLib20U2/LIBRARY/src/
 wget https://raw.githubusercontent.com/weolar/miniblink49/master/vc6/include/crt/float.h
 cd ..
 make CC_NAME_INDEX=3 CC_INDEX=3 _HOST_OS=Windows_NT _HOST_ARCH=x86 _NUM_CPUS=1 CC=i686-w64-mingw32-gcc CFLAGS_OPT="-O2 -DBID_THREAD= -DBID_MS_FLAGS" CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
@@ -33,7 +33,7 @@ cp libbid.dll $prefix/bin/.
 elif [ $target == "i686-linux-gnu" ]; then
 
 cd $WORKSPACE/srcdir
-cd IntelRDFPMathLib20U1/LIBRARY/
+cd IntelRDFPMathLib20U2/LIBRARY/
 make _HOST_ARCH=i686 CC=gcc CFLAGS_OPT="-O2 -fPIC" CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
 $CC -shared -o libbid.so *.o
 mkdir $prefix/lib
@@ -42,7 +42,7 @@ cp libbid.* $prefix/lib/.
 elif [ $target == "x86_64-apple-darwin14" ]; then
 
 cd $WORKSPACE/srcdir
-cd IntelRDFPMathLib20U1/LIBRARY/
+cd IntelRDFPMathLib20U2/LIBRARY/
 make CC=gcc CFLAGS_OPT="-O2 -fPIC" CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
 $CC -shared -o libbid.dylib *.o
 mkdir $prefix/lib
@@ -51,7 +51,7 @@ cp libbid.* $prefix/lib/.
 else
 
 cd $WORKSPACE/srcdir
-cd IntelRDFPMathLib20U1/LIBRARY/
+cd IntelRDFPMathLib20U2/LIBRARY/
 make CC=gcc CFLAGS_OPT="-O2 -fPIC" CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
 $CC -shared -o libbid.so *.o
 mkdir $prefix/lib
