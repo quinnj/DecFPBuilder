55c9a5c2c4bfa5853e9ee3b477383d868f25b139
Simon Byrne <simonbyrne@gmail.com>
2019-03-03 21:49:07 -0800

    Simplify build script

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,77 +1,52 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-name = "DecFP"
-version = v"2.0.2" # 2.0 Update 2
+name = "decfp2"
+version = v"2.2.0" # 2.0 Update 2
 
-# Collection of sources required to build DecFP
+# Collection of sources required to build decfp2
 sources = [
     "https://www.netlib.org/misc/intel/IntelRDFPMathLib20U2.tar.gz" =>
     "93c0c78e0989df88f8540bf38d6743734804cef1e40706fd8fe5c6a03f79e173",
+
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-if [ $target == "x86_64-w64-mingw32" ]; then
-
-cd $WORKSPACE/srcdir
-cd IntelRDFPMathLib20U2/LIBRARY/src/
-wget https://raw.githubusercontent.com/weolar/miniblink49/master/vc6/include/crt/float.h
-cd ..
-make CC_NAME_INDEX=3 CC_INDEX=3 _HOST_OS=Windows_NT _HOST_ARCH=x86_64 _NUM_CPUS=1 CC=x86_64-w64-mingw32-gcc CFLAGS_OPT="-O2 -DBID_THREAD= -DBID_MS_FLAGS" CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
-$CC -shared -o libbid.dll *.obj
-mkdir $prefix/bin
-cp libbid.dll $prefix/bin/.
-
-elif [ $target == "i686-w64-mingw32" ]; then
-
-cd $WORKSPACE/srcdir
-cd IntelRDFPMathLib20U2/LIBRARY/src/
-wget https://raw.githubusercontent.com/weolar/miniblink49/master/vc6/include/crt/float.h
-cd ..
-make CC_NAME_INDEX=3 CC_INDEX=3 _HOST_OS=Windows_NT _HOST_ARCH=x86 _NUM_CPUS=1 CC=i686-w64-mingw32-gcc CFLAGS_OPT="-O2 -DBID_THREAD= -DBID_MS_FLAGS" CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
-$CC -shared -o libbid.dll *.obj
-mkdir $prefix/bin
-cp libbid.dll $prefix/bin/.
-
-elif [ $target == "i686-linux-gnu" ]; then
-
 cd $WORKSPACE/srcdir
 cd IntelRDFPMathLib20U2/LIBRARY/
-make _HOST_ARCH=i686 CC=gcc CFLAGS_OPT="-O2 -fPIC" CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
-$CC -shared -o libbid.so *.o
-mkdir $prefix/lib
-cp libbid.* $prefix/lib/.
-
-elif [ $target == "x86_64-apple-darwin14" ]; then
-
-cd $WORKSPACE/srcdir
-cd IntelRDFPMathLib20U2/LIBRARY/
-make CC=gcc CFLAGS_OPT="-O2 -fPIC" CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
-$CC -shared -o libbid.dylib *.o
-mkdir $prefix/lib
-cp libbid.* $prefix/lib/.
-
+if [ "$nbits" == 64 ]; then
+     _HOST_ARCH=x86_64
 else
-
-cd $WORKSPACE/srcdir
-cd IntelRDFPMathLib20U2/LIBRARY/
-make CC=gcc CFLAGS_OPT="-O2 -fPIC" CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
-$CC -shared -o libbid.so *.o
-mkdir $prefix/lib
-cp libbid.* $prefix/lib/.
-
+     _HOST_ARCH=x86
 fi
+
+if [[ $target == *"-w64-"* ]]; then
+    make CC_NAME=cc _HOST_OS=Windows_NT _HOST_ARCH=$_HOST_ARCH AR_CMD="ar rv" CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
+    $CC -shared -o libbid.$dlext *.obj
+else
+    make CC_NAME=cc CFLAGS_OPT=-fPIC CFLAGS=-fPIC _HOST_ARCH=$_HOST_ARCH CALL_BY_REF=0 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=1
+    $CC -shared -o libbid.$dlext *.o
+fi
+
+mkdir -p $prefix/bin
+cp libbid.$dlext $prefix/bin/
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Linux(:i686, :glibc),
-    Linux(:x86_64, :glibc),
-    # Linux(:aarch64, :glibc),
-    # Linux(:armv7l, :glibc),
-    # Linux(:powerpc64le, :glibc),
-    MacOS(),
+    Linux(:i686, libc=:glibc),
+    Linux(:x86_64, libc=:glibc),
+    Linux(:aarch64, libc=:glibc),
+    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    Linux(:powerpc64le, libc=:glibc),
+    Linux(:i686, libc=:musl),
+    Linux(:x86_64, libc=:musl),
+    Linux(:aarch64, libc=:musl),
+    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
+    MacOS(:x86_64),
     Windows(:i686),
     Windows(:x86_64)
 ]
@@ -83,7 +58,9 @@ products(prefix) = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
+
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
